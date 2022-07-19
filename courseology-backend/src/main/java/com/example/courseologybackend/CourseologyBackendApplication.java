556e80da081a335b311cc80a78ac0bfb2b1493d2
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-07-19 12:39:18 +0100

    created file structure

---
+++ courseology-backend/src/main/java/com/example/courseologybackend/CourseologyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseologybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseologyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseologyBackendApplication.class, args);
+	}
+
+}
---
+++ courseology-backend/src/test/java/com/example/courseologybackend/CourseologyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseologybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseologyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
